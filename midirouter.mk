fcbfbc52a013b8058fc55c820093a3b4a42d43b4
Oil Can Steve <oilcansteve@hotmail.com>
2026-01-24 18:29:54 +0000

    first commit

---
+++ midirouter.mk
@@ -0,0 +1,21 @@
+######################################
+#
+# MIDIROUTER
+#
+######################################
+MIDIROUTER_VERSION = 4478ead1bb8114f41a61603f43a6147af368f8b3
+MIDIROUTER_SITE = https://github.com/stivb/midirouter.git
+MIDIROUTER_SITE_METHOD = git
+MIDIROUTER_BUNDLES = midirouter.lv2
+
+MIDIROUTER_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) MOD=1 OPTIMIZATIONS="" PREFIX=/usr -C $(@D)/source
+
+define MIDIROUTER_BUILD_CMDS
+	$(MIDIROUTER_TARGET_MAKE)
+endef
+
+define MIDIROUTER_INSTALL_TARGET_CMDS
+	$(MIDIROUTER_TARGET_MAKE) install DESTDIR=$(TARGET_DIR)
+endef
+
+$(eval $(generic-package))
